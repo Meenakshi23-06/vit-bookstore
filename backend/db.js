0915dfd039436fbd9b5bd3704675ee86d93528f7
Meena <meenakshipoolaa@gmail.com>
2026-04-10 17:14:32 +0530

    Fixed Neo4j Username credential

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const neo4j = require("neo4j-driver");
 
-const URI = process.env.NEO4J_URI || "neo4j+s://10c966a8.databases.neo4j.io";
-const USER = process.env.NEO4J_USER || "neo4j";
-const PASSWORD = process.env.NEO4J_PASSWORD || "AzzUBjQZGJrpwdr2pi0oOBg_4557fSluOesY2_Ybo3Q";
+const URI = "neo4j+s://10c966a8.databases.neo4j.io";
+const USER = "10c966a8";
+const PASSWORD = "AzzUBjQZGJrpwdr2pi0oOBg_4557fSluOesY2_Ybo3Q";
 
 const driver = neo4j.driver(
   URI,
